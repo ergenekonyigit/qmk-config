94a65765b0a8c484e2071edfea26c79ed244054f
ergenekonyigit <yigitergenekon@gmail.com>
2023-08-19 02:19:37 +0300

    update planck

--- planck/config.h
+++ planck/config.h
@@ -18,16 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID 0xFEED
-#define PRODUCT_ID 0x0000
-#define DEVICE_VER 0x0001
-#define MANUFACTURER ErgenekonYigit
-#define PRODUCT Glosso
 #define UNICODE_SELECTED_MODES UC_MAC
-
-/* key matrix size */
-#define MATRIX_ROWS 4
-#define MATRIX_COLS 12
 
 /*
  * Keyboard Matrix Assignments
